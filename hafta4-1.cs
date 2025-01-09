2e66b2a8f6238e9c251b933f676fd65e0cbd460f
smelikeaslan <aslansumeyramelike@gmail.com>
2025-01-09 16:02:54 +0300

    Add files via upload

---
+++ hafta4-1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+       
+        Console.Write("Üçgenin taban uzunluğunu girin: ");
+        double taban = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Üçgenin yüksekliğini girin: ");
+        double yukseklik = Convert.ToDouble(Console.ReadLine());
+
+       
+        double alan = UcgenAlaniHesapla(taban, yukseklik);
+
+        
+        Console.WriteLine($"Üçgenin alanı: {alan}");
+    }
+
+    
+    static double UcgenAlaniHesapla(double taban, double yukseklik)
+    {
+        return (taban * yukseklik) / 2;
+        Console.ReadKey();
+    }
+    
+}
